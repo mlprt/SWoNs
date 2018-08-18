8039d5fcca06ba1431bd8bdcbd63631fee8e4e22
Matt L Laporte <matt.l.laporte@gmail.com>
2018-08-18 15:59:45 -0400

    Plot incorporating decision time histogram and r evolution over time

--- pooledInhibitionPlots.m
+++ pooledInhibitionPlots.m
@@ -62,6 +62,70 @@ for q = 1:250
 
 end
 
+%% r traces + DT histogram
+nBins = 150;
+set(0, 'defaultAxesFontSize', 12);
+lineAlpha = 0.35;
+
+% oolours
+colors = lines(2);
+c1 = colors(1, :);
+c2 = colors(2, :);
+
+% subplot positions
+left = 0.1;
+top = 0.9;
+bottom1 = 0.75;
+bottom2 = 0.15;
+width=0.85;
+right = 0.95;
+height=0.2;
+
+figure('Color', 'w', 'Position', [800 100 750 600]);
+
+ax1 = axes('Position', [left bottom1 right-left top-bottom1]);
+%subplot(2,1,1); hold on; 
+ylabel('p(D)')
+rtNonZero = rt;
+rtNonZero(rtNonZero == 0) = nan; %maxsteps+1;
+[rtCounts, rtCenters] = hist(rtNonZero, nBins);
+totalCount = sum(cumsum(rtCounts), 2);
+xCutoff = find(totalCount > totalCount(end) - 1, 1);
+bar1 = bar(rtCenters, rtCounts / totalCount(end), 1);
+bar1(1).FaceColor = c1;
+bar1(2).FaceColor = c2;
+xlim([0 xCutoff])
+set(gca, 'XTickLabel', [],'XTick',[], 'YTick', [])
+hAx = gca;
+hAx.YRuler.Axle.LineStyle = 'none';
+hAx.XRuler.Axle.LineStyle = 'none';
+legend('L', 'R', 'Location', 'northeast')
+legend('boxoff')
+
+%subplot(2,1,2); hold on; 
+ax2 = axes('Position', [left bottom2 right-left bottom1-bottom2]); 
+hold on;
+for q = 1:size(r, 2)
+    rs = r{q};
+    plotSteps = size(rs, 2);
+    plot1 = plot(1:plotSteps, rs(1, :));
+    plot2 = plot(1:plotSteps, rs(2, :));
+    plot1.Color(1:3) = c1;
+    plot1.Color(4) = lineAlpha;
+    plot2.Color(1:3) = c2;
+    plot2.Color(4) = lineAlpha;
+end
+xlim([0 xCutoff])
+xlabel('Timestep')
+ylabel('r')
+hAx = gca;
+hAx.BoxStyle = 'full';
+ylim([-0 thres])
+set(gca, 'YTick', [0 0.5 thres])
+hAx.YRuler.Axle.LineStyle = 'solid';
+hAx.XRuler.Axle.LineStyle = 'solid';
+box on;
+
 
 %% sequential decision analysis
 % decision = zeros(seqTrials, nNets, nSeq, 2);
