fd38d22597366288a0f47bc7e1130c8644acf3b7
Matt L Laporte <matt.l.laporte@gmail.com>
2018-08-20 16:18:46 -0400

    Removed unzipped master branch someone must have downloaded into the repository at some point

--- SWoNs-master/dottheta.m
+++
@@ -1,3 +0,0 @@
-function f = dottheta(omega, lambda, r, psi, theta)
-f = omega + lambda * r * sin(psi - theta);
-end
--- SWoNs-master/kuramoto.m
+++
@@ -1,6 +0,0 @@
-%% Kuramoto Oscillator
-function f=kuramoto(x,K,N,Omega)
- 
-f=Omega+(K/N)*sum(sin(x*ones(1,N)-(ones(N,1)*x')))';
- 
-end
